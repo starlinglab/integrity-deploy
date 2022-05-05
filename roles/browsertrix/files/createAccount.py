bb1d84d726b59e15fa823bcea6e817e5a282ec59
YurkoWasHere <YurkoWasHere@users.noreply.github.com>
2022-05-05 14:48:10 -0400

    createAccount now can make multiple browsertrix accounts

--- roles/browsertrix/files/createAccount.py
+++ roles/browsertrix/files/createAccount.py
@@ -1,18 +1,27 @@
 #!/usr/bin/python3
 import json
 import requests
+import sys
 
-email = "{{ browsertrix_user_email }}"
-password = "{{ browsertrix_user_password }}"
+if len(sys.argv) < 4:
+    print (f"usage: {sys.argv[0]} username@domain.com password name_of_archive [invite_user")
+    sys.exit(-1)
 
+email = sys.argv[1]
+password = sys.argv[2]
+archive_name = sys.argv[3]
+
+invite=""
+if len(sys.argv) == 5:
+    invite = sys.argv[4]
 
 URL = "http://127.0.0.1:9871/api/auth/register"
 body = {
     "email": email,
     "password": password,
-    "name": "Integrity Account",
+    "name": archive_name,
     "newArchive": True,
-    "newArchiveName": "Integrity Archive",
+    "newArchiveName": archive_name
 }
 response = requests.post(URL, json=body)
 
@@ -31,4 +40,13 @@ headers = {"Authorization": "Bearer " + access_token}
 URL = "http://127.0.0.1:9871/api/archives"
 r = requests.get(URL, headers=headers)
 res = r.json()
-print(res["archives"][0]["id"])
+aid = res["archives"][0]["id"]
+
+if (invite !=""):
+    d={ "email": invite, "role":10 }
+    # get AID
+    URL = f"http://127.0.0.1:9871/api/archives/{aid}/invite/"
+    r = requests.post(URL, headers=headers, json=d)
+    res = r.json()
+
+echo aid
