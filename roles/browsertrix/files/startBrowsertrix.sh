346d86a751b159bc3d19737c1a9393fb28ee2e4e
YurkoWasHere <YurkoWasHere@users.noreply.github.com>
2022-07-27 07:50:45 -0400

    More swarm patches

--- roles/browsertrix/files/startBrowsertrix.sh
+++ roles/browsertrix/files/startBrowsertrix.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 cd /root/browsertrix
+docker swarm init
+
+set -o allexport
 source configs/config.env
+
 docker stack deploy \
 -c /root/browsertrix/docker-compose.yml \
 -c /root/browsertrix/configs/docker-compose.swarm.yml \
